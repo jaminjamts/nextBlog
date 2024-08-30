995bf24915b51da9a7c11601b3a7c39c4630eb34
Жаминжамц Батсундуй <24LP6854@G401-CC0094.local>
2024-08-30 12:53:58 +0800

    30

---
+++ src/components/accessories/MainContent.jsx
@@ -0,0 +1,7 @@
+export const MainContent = ({ content }) => {
+  return (
+    <main className="text-xl md:text-xl  text-[var(--text-black)] px-4">
+      {content}
+    </main>
+  );
+};
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,24 +1,78 @@
+import { useState } from "react";
 import { BlueTitle, CreateAt } from "../accessories";
+import { MainContent } from "../accessories/MainContent";
 
 export const Carousel = () => {
-  const data = [{ title: "dasdca", createAt: "2024.12.24", content: "" }];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const data = [
+    {
+      index: 0,
+      url: "https://images.unsplash.com/photo-1569580388550-50907e7bad86?q=80&w=5148&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "technology",
+      createAt: "2024.12.24",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. OptioLorem ipsum dolor ",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1479888230021-c24f136d849f?q=80&w=5070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "technology",
+      createAt: "2024.12.24",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. OptioLorem ipsum dolor ",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1488841714725-bb4c32d1ac94?q=80&w=5030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "technology",
+      createAt: "2024.12.24",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. OptioLorem ipsum dolor ",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1569580388550-50907e7bad86?q=80&w=5148&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "technology",
+      createAt: "2024.12.24",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. OptioLorem ipsum dolor ",
+    },
+  ];
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? data.length - 1 : currentIndex - 1;
+  };
   return (
     <main className="w-screen h-[50vh] flex justify-center">
       <div className="container h-full">
-        <div className='bg-[url("/image.jpg")] h-full bg-cover rounded-2xl bg-right flex items-end p-2'>
-          {data.map((item) => (
-            <div className="bg-white flex flex-col h-1/2 w-1/2 rounded-2xl">
-              <div className="flex justify-around py-4">
+        {data.map((item) => (
+          <div className="h-full w-full relative">
+            <div
+              className="h-full w-full"
+              style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+            >
+              <img
+                src={item.url}
+                alt="main zurag"
+                className="h-full w-full rounded-2xl "
+              />
+            </div>
+            <div className="bg-white flex flex-col w-1/2 h-1/2 rounded-2xl absolute bottom-5 left-5 justify-center">
+              <div className="flex justify-between px-14 py-4">
                 <BlueTitle detailsTitle={item.title} />
                 <CreateAt date={item.createAt} />
               </div>
-              <div>{item.content}</div>
+              <div className="flex px-10">
+                <MainContent content={item.content} />
+              </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
         <div className="flex justify-center gap-2 pt-2">
-          <button className="border p-4 rounded-md">{"<"}</button>
-          <button className="border p-4 rounded-md">{">"}</button>
+          <button className="border p-4 rounded-md pointer" onClick={prevSlide}>
+            {"<"}
+          </button>
+          <button className="border p-4 rounded-md pointer" onClick={nextSlide}>
+            {">"}
+          </button>
         </div>
       </div>
     </main>
